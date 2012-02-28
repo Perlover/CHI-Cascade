4868bcf2f28e301229910e9b2c13730ee963e061
Perlover <perlover@perlover.com>
2012-02-28 23:30:36 +0500

    Some improvements of tests with queue & state

--- t/lib/test_04.pm
+++ t/lib/test_04.pm
@@ -36,39 +36,45 @@ sub test_cascade {
 	recomputed	=> sub { $recomputed++ }
     );
 
-    ok( $cascade->{stats}{recompute} == 0, 'recompute stats - 1');
+    ok( $cascade->{stats}{recompute} == 0 );
 
     my $state;
 
     my $time1 = time;
-    ok( ! defined $cascade->run( 'one_page_0', queue => 'test', state => \$state ), '0th page, queued');
+    ok( ! defined $cascade->run( 'one_page_0', queue => 'test', state => \$state ) );
     my $time2 = time;
 
-    ok( $cascade->{stats}{recompute} == 0, 'recompute stats - 2');
-    ok( $time2 - $time1 < 0.1, 'time of 1st starting' );
+    ok( $cascade->{stats}{recompute} == 0 );
+    ok( $time2 - $time1 < 0.1 );
     ok( CHI::Cascade::Value->state_as_str($state) eq "CASCADE_NO_CACHE | CASCADE_QUEUED" );
 
     my $res;
 
     $time1 = time;
-    ok ( $cascade->queue('test') == 1, '1st queue run' );
+    ok ( $cascade->queue('test') == 1 );
     $time2 = time;
-    ok( $time2 - $time1 > 0.9 && $time2 - $time1 < 1.1, 'time of queue' );
+    ok( $time2 - $time1 > 0.9 && $time2 - $time1 < 1.1 );
 
-    ok( defined( $res = $cascade->run( 'one_page_0', queue => 'test', state => \$state ) ), '0th page, not queued because recomputed already');
+    ok( defined( $res = $cascade->run( 'one_page_0', queue => 'test', state => \$state ) ) );
     is_deeply( $res, [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] );
     ok( CHI::Cascade::Value->state_as_str($state) eq "CASCADE_ACTUAL_VALUE | CASCADE_FROM_CACHE" );
 
-    ok ( $cascade->queue('test') == 0, 'second queue run' );
+    ok ( $cascade->queue('test') == 0 );
 
-    ok( defined( $res = $cascade->run( 'one_page_1', state => \$state ) ), '1th page, not queued' );
-    is_deeply( $res, [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ], 'result of 1st page' );
+    ok( defined( $res = $cascade->run( 'one_page_1', state => \$state ) ) );
+    is_deeply( $res, [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ] );
     ok( CHI::Cascade::Value->state_as_str($state) eq "CASCADE_ACTUAL_VALUE | CASCADE_RECOMPUTED" );
 
-    ok( defined( $res = $cascade->run( 'one_page_1', queue => 'test', state => \$state ) ), '1th page, queued' );
-    is_deeply( $res, [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ], 'result of 1st page' );
+    ok( defined( $res = $cascade->run( 'one_page_1', queue => 'test', state => \$state ) ) );
+    is_deeply( $res, [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ] );
     ok( CHI::Cascade::Value->state_as_str($state) eq "CASCADE_ACTUAL_VALUE | CASCADE_FROM_CACHE" );
-    ok ( $cascade->queue('test') == 0, '3rd queue run' );
+    ok ( $cascade->queue('test') == 0 );
+
+    $cascade->target_remove( 'one_page_1' );
+    ok( defined( $res = $cascade->run( 'one_page_1', queue => 'test', state => \$state ) ) );
+    is_deeply( $res, [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ] );
+    ok( CHI::Cascade::Value->state_as_str($state) eq "CASCADE_FROM_CACHE | CASCADE_QUEUED" );
+    ok ( $cascade->queue('test') == 1 );
 }
 
 1;
